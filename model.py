3b1229963e994cc4d856873719ec9f7a201f1e79
MilkShaikh3 <abdshaikh33@gmail.com>
2020-06-02 12:31:06 +0200

    Add changes

--- model.py
+++ model.py
@@ -59,8 +59,80 @@ def _preprocess_data(data):
     # ---------------------------------------------------------------
 
     # ----------- Replace this code with your own preprocessing steps --------
-    predict_vector = feature_vector_df[['Pickup Lat','Pickup Long',
-                                        'Destination Lat','Destination Long']]
+
+    dataset1 = pd.read_csv('/Users/abdsh/Desktop/regression-predict-api-template/utils/data/train_data.csv', index_col=0)
+    dataset2 = pd.read_csv('/Users/abdsh/Desktop/regression-predict-api-template/utils/data/riders.csv', index_col=0)
+
+    train = pd.merge(dataset1, dataset2, how='left', on='Rider Id') 
+
+    train.columns = [col.replace(" - "," ") for col in train.columns]
+    train.columns = [col.replace(" ","_") for col in train.columns]
+    train.columns = [col.replace("_(Mo_=_1)","") for col in train.columns]
+    train.columns = [col.replace("(KM)","KM") for col in train.columns]
+
+    def convert_time(input_df, column_name):
+        
+        input_df[column_name] = pd.to_datetime(input_df[column_name]).dt.hour
+        bins=[-1, 3, 7, 11, 15, 19, 23]
+        labels=pd.Categorical(['Night', 'EarlyMorning', 'Morning', 'Afternoon', 'Evening', 'Night'])
+        input_df[column_name] = pd.cut(x=input_df[column_name], bins=bins, labels=labels)
+  
+        return 
+
+    convert_time(train, 'Pickup_Time')
+    convert_time(train, 'Placement_Time')
+    convert_time(train, 'Confirmation_Time')
+    convert_time(train, 'Arrival_at_Pickup_Time')
+    convert_time(train, 'Arrival_at_Destination_Time')
+
+    columns = ['Personal_or_Business', 'Platform_Type', 'Arrival_at_Pickup_Time', 
+           'Confirmation_Time', 'Placement_Time', 'Pickup_Time']
+           
+    train_dummies = pd.get_dummies(train, columns=columns, drop_first=True) 
+
+    train_dummies.columns = [col.replace(" ", "_") for col in train_dummies.columns]
+
+    column_titles = [col for col in train_dummies.columns if col!= 'Time_from_Pickup_to_Arrival'] + ['Time_from_Pickup_to_Arrival']
+    train_dummies=train_dummies.reindex(columns=column_titles)
+
+    y_data = train_dummies['Time_from_Pickup_to_Arrival']
+
+    X_names = ['Distance_KM', 'No_Of_Orders', 'Average_Rating', 
+           'Personal_or_Business_Personal','Pickup_Time_EarlyMorning', 
+           'Pickup_Time_Evening', 'Pickup_Time_Morning','Pickup_Time_Night', 
+           'Arrival_at_Pickup_Time_EarlyMorning',
+           'Arrival_at_Pickup_Time_Evening', 'Arrival_at_Pickup_Time_Morning', 
+           'Arrival_at_Pickup_Time_Night']               
+
+    X_data = train_dummies[X_names]
+
+    X_remove = ['Arrival_at_Pickup_Time_EarlyMorning', 'Arrival_at_Pickup_Time_Evening', 'Arrival_at_Pickup_Time_Morning',
+                'Arrival_at_Pickup_Time_Night']
+    X_corr_names = [col for col in X_names if col not in X_remove]
+
+    y_train = train_dummies['Time_from_Pickup_to_Arrival']
+    X_train = train_dummies[X_corr_names]
+
+    dataset3 = pd.read_csv('/Users/abdsh/Desktop/regression-predict-api-template/utils/data/test_data.csv', index_col=0)
+
+    test = pd.merge(dataset3, dataset2, how='left', on='Rider Id') 
+
+    test.columns = [col.replace(" - "," ") for col in test.columns]
+    test.columns = [col.replace(" ","_") for col in test.columns]
+    test.columns = [col.replace("_(Mo_=_1)","") for col in test.columns]
+    test.columns = [col.replace("(KM)","KM") for col in test.columns]\
+    
+    convert_time(test, 'Pickup_Time')
+
+    columns = ['Personal_or_Business', 'Arrival_at_Pickup_Time', 'Pickup_Time']
+    test_dummies = pd.get_dummies(test, columns=columns, drop_first=True) 
+
+    test_dummies.columns = [col.replace(" ", "_") for col in test_dummies.columns]
+
+    X_test = test_dummies[X_corr_names]
+    
+    predict_vector = X_test
+
     # ------------------------------------------------------------------------
 
     return predict_vector
